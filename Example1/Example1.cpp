99d5958d477d8d23661dc9c3d41073416076c13b
Anders Modén <anders.moden@saabgroup.com>
2023-01-20 09:20:59 +0100

    Updated nugets

--- Example1/Example1.cpp
+++ Example1/Example1.cpp
@@ -8,12 +8,14 @@ int main()
 {
     std::cout << "Hello World!\n";
 
+    gzGooble::setDecimalPrecision(100);
+
     gzGooble a("123.456789817264817248");
 
     gzGooble b("29835923857298357928357928357298523234.23423423742837628");
 
 
-    std::cout << (const char*)a.asString()<<"+"<< (const char*)b.asString()<<"="<< (const char*)(a+b).asString()<<"\n";
+    std::cout << (const char*)a.round().asString()<<"+"<< (const char*)b.round().asString()<<"="<< (const char*)(a+b).round().asString()<<"\n";
 
 }
 
